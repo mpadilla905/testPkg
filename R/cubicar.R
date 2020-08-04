44c278d022ad9044e079c9062b9138a7c2d05a62
mpadilla <mpadilla@lcgej.unam.mx>
2020-08-04 16:57:51 -0500

    añadí la función cubicar y su test

---
+++ R/cubicar.R
@@ -0,0 +1,5 @@
+cubicar = function(n){
+  if(is.na(n))
+    stop("no se soportan NAs")
+  n^3
+}
--- tests/testthat/test-cubicar.R
+++ tests/testthat/test-cubicar.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("cubicar funciona", {
+  expect_equal(
+    cubicar(2), 8
+  )
+  expect_error(
+    cubicar(NA)
+  )
 })
